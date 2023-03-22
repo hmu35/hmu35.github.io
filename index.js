13ec865426a1cebfadbe056319b537ea9ca870be
unknown <h406627@gamil.com>
2023-03-22 14:52:33 +0530

    hi

--- index.js
+++ index.js
@@ -6,32 +6,15 @@ function openmenu(){
 function closemenu(){
     sidemenu.style.right="-200px";
 }
-//  // typing text animation script
-//  var typed = new Typed(".typing", {
-//   strings: ["Full Stack  Developer"],
-//   typeSpeed: 100,
-//   backSpeed: 60,
-//   loop: true,
-// })
-
-// document.getElementById('resume-button-1').addEventListener("click",()=>{
-//   // console.log("OPENinig.....")
-//   window.location.assign("https://drive.google.com/file/d/10C5IVHqeM2huu0F-KyZHNAo3XD0VGp9w/view?usp=share_link", "_blank");
-// })
-
 document.getElementById('resume-link-1').addEventListener("click",()=>{
-  // console.log("OPENinig.....")
+
   window.location.assign("https://drive.google.com/file/d/1aAKXgCvio3Qej4nPxjhuix-biEL_unHw/view?usp=sharing", "_blank");
 })
 
 document.getElementById('resume-link-2').addEventListener("click",()=>{
-  // console.log("OPENinig.....")
+ 
   window.location.assign("https://drive.google.com/file/d/1aAKXgCvio3Qej4nPxjhuix-biEL_unHw/view?usp=sharing", "_blank");
 })
 
-// sr.reveal('#home',{})
-// sr.reveal('#main-cont p',{delay:200})
-// sr.reveal('#main-cont h1',{delay:300})
-// sr.reveal('.container',{delay:400})
-// sr.reveal('#resume-button-2',{interval:500})
+
 
